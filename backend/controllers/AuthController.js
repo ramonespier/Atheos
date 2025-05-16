ca4b5f2fd050d62f3937679be4d88015b54956ca
ramonespier <ramon.coelhomelo@outlook.com>
2025-05-16 11:19:57 -0300

    chore: identação

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -1,42 +1,6 @@
-// import jwt from 'jsonwebtoken';
-// import { JWT_SECRET } from '../config/jwt.js';
-
-/*
-
-import { read } from '../config/database.js';
-
-const loginController = async (req, res) => {
-
-    const { email, senha } = req.body;
-    try {
-        const usuario = await read('usuarios', `email = '${email}'`);
-        if (!usuario) {
-          return res.status(404).json({message: 'Usuário não encontrado'});
-        }
-        const senhaCorreta = await read('usuarios', `senha = ${senha}`);
-
-        if(!senhaCorreta){
-            return res.status(401).json({message: 'Senha incorreta'})
-        }
-
-        // const token = jwt.sign({id:usuario.id, tipo: usuario.tipo}, JWT_SECRET, { expiresIn: '1h'})
-
-        res.status(200).json({message: 'Login efetuado com sucesso.' })
-
-    } catch (err){
-        console.error('Erro ao fazer login: ', err)
-        res.status(500).json({message: 'Erro ao fazer login'})
-    }
-
-};
-
-export { loginController }
-
-*/
-
 import jwt from 'jsonwebtoken';
 import { read, compare } from '../config/database.js';
-import { JWT_SECRET } from '../config/jwt.js'; // Importar a chave secreta
+import { JWT_SECRET } from '../config/jwt.js'; 
 
 const loginController = async (req, res) => {
     const { email, senha } = req.body;
@@ -53,9 +17,7 @@ const loginController = async (req, res) => {
             return res.status(401).json({ mensagem: 'Senha incorreta' });
         }
 
-        const token = jwt.sign({ email: usuario.email, senha: usuario.senha }, JWT_SECRET, {
-            expiresIn: '1h',
-        });
+        const token = jwt.sign({ email: usuario.email, senha: usuario.senha }, JWT_SECRET, {expiresIn: '1h'});
 
     res.json({ mensagem: 'Login realizado com sucesso', token });
 } catch (error) {
