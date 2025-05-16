2e4198956e1b0ea3c28983854081cbbc6b71861b
ramonespier <ramon.coelhomelo@outlook.com>
2025-05-16 11:19:35 -0300

    chore: identação

--- backend/config/database.js
+++ backend/config/database.js
@@ -5,7 +5,7 @@ const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'teste_atheos',
+    database: 'Atheos',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
