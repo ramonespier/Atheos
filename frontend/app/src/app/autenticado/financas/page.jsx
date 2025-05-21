d7bb94b562fa9e7ff495a0fc461f5085e304cdab
ramonespier <ramon.coelhomelo@outlook.com>
2025-05-21 14:37:10 -0300

    test: teste para rota que requer login (sucesso)

---
+++ frontend/app/src/app/autenticado/financas/page.jsx
@@ -0,0 +1,24 @@
+"use client"
+
+// teste para rota que requer login
+
+import { useEffect } from "react";
+
+export default function Financas() {
+
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+
+        if (!token) {
+            setTimeout(() => {
+                window.location.href = '/'
+            }, 600);
+            return;
+        }
+    })
+
+
+    return (
+        <div>oiii</div>
+    )
+}
