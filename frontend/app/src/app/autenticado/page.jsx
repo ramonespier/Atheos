c72c96175c2a1dc5b0d023dcddef63475828781b
ramonespier <ramon.coelhomelo@outlook.com>
2025-05-21 17:09:51 -0300

    feat: funçao que impede de acessar demais páginas sem login e redução de tempo de acesso

--- frontend/app/src/app/autenticado/page.jsx
+++ frontend/app/src/app/autenticado/page.jsx
@@ -1,21 +1,29 @@
 "use client"
+
 import { useEffect, useState } from "react";
+
+function disconnect() {
+    localStorage.removeItem('token')
+    window.location.href = '/'
+}
+
 export default function Autenticado() {
   const [usuario, setUsuario] = useState(null);
   const [carregando, setCarregando] = useState(true);
-
+  
+  
   useEffect(() => {
     const token = localStorage.getItem('token');
-
+    
     if (!token) {
       setTimeout(() => {
         window.location.href = '/'
-      }, 2000);
+      }, 600);
       return;
     }
-
+    
     setCarregando(true);
-
+    
     fetch('http://localhost:3001/usuario/autenticado', {
       method: 'GET',
       headers: {
@@ -23,37 +31,41 @@ export default function Autenticado() {
         'Content-Type': 'application/json'
       }
     })
-      .then(res => {
-        console.log('Status da resposta: ', res.status)
-        if (!res.ok) throw new Error('Falha na autenticação');
-        return res.json();
-      })
-      .then(data => {
-        setUsuario(data);
-        setCarregando(false);
-      })
-      .catch(err => {
-        console.error('Erro:', err);
-        setTimeout(() => {
-          window.location.href = '/'
-        }, 2000);
-      });
+    .then(res => {
+      console.log('Status da resposta: ', res.status)
+      if (!res.ok) throw new Error('Falha na autenticação');
+      return res.json();
+    })
+    .then(data => {
+      setUsuario(data);
+      setCarregando(false);
+    })
+    .catch(err => {
+      console.error('Erro:', err);
+      setTimeout(() => {
+        window.location.href = '/'
+      }, 600);
+    });
   }, []);
-
+  
   if (carregando) {
     return <div>Carregando...</div>;
   }
-
+  
   if (!usuario) {
     return <div>Não foi possível carregar os dados do usuário</div>;
   }
   console.log(usuario)
-
+  
   return (
     <div>
       <h1>Bem-vindo!</h1>
       <p>Email: {usuario.email}</p>
-      {usuario.nome && <p>Nome: {usuario.nome}</p>}
+      <p>Nome: {usuario.nome}</p>
+      <p>id: {usuario.id}</p>
+      <p>criado_em: {usuario.criado_em}</p>
+
+      <span onClick={disconnect}>Desconectar</span>
     </div>
   );
 }
