f39efc50a7376d2c0a2181fea430a67e28cca688
ramonespier <ramon.coelhomelo@outlook.com>
2025-05-22 10:15:54 -0300

    pasta components

---
+++ frontend/app/src/app/components/HeaderHome/HeaderHome.jsx
@@ -0,0 +1,13 @@
+"use client"
+// import { useState, useEffect } from "react";
+
+export default function HeaderHome() {
+    <div>
+        <header
+            className="
+            h-56
+            w-full
+            bg-slate-700">
+        </header>
+    </div>
+}
