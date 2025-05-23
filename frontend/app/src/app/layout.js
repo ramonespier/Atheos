a3016524718765c4ba62b5db8fc4f3421dcdabb4
FantaUva350ml <davi2008rodriguespires@gmail.com>
2025-05-23 11:19:32 -0300

    atibuição adequada de site ao navegador, flaticon adicionado

--- frontend/app/src/app/layout.js
+++ frontend/app/src/app/layout.js
@@ -20,7 +20,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased w-screen h-screen `}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased  h-screen `}
       >
         {children}
       </body>
