8971f1c0d97881371e6b49c754c16f30ad6cf52f
DarkMetter-Samuel <sajuan1020@gmail.com>
2025-06-09 11:19:31 -0300

    Style: cor banner

--- frontend/app/src/app/components/Home/BannerChamada.jsx
+++ frontend/app/src/app/components/Home/BannerChamada.jsx
@@ -1,13 +1,14 @@
 export default function BannerChamada() {
     return (
-        <section  className="w-full bg-gradient-to-r from-indigo-700 via-indigo-500 to-indigo-700 py-14 flex flex-col items-center gap-6 text-white">
+        <section className="w-full bg-gradient-to-r from-orange-700 to-amber-600 py-14 flex flex-col items-center gap-6 text-white">
             <h2 className="text-4xl md:text-6xl font-bold text-center">
                 Junte-se aos Deuses!
             </h2>
             <p className="text-lg md:text-xl text-center max-w-2xl">
                 Torne-se mestre das suas finanças com Atheos. Comece agora sua jornada rumo à sabedoria divina!
             </p>
-            <a id="depoimentos" href="/login" className="bg-white text-indigo-700 font-semibold py-3 px-8 rounded-xl hover:bg-indigo-100 transition-all">
+            {/* O botão continua claro, criando um ótimo ponto de foco */}
+            <a id="depoimentos" href="/login" className="bg-white text-orange-700 font-semibold py-3 px-8 rounded-xl hover:bg-orange-50 transition-all">
                 Fazer Login/Cadastro
             </a>
         </section>
